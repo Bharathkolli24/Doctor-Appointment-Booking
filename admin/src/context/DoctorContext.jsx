85bcadcee689cff78af6cfe5dfcd38e76f16fe51
VIJAYA NAGA BHARATH KOLLI <bharathkolli24@gmail.com>
2025-11-28 18:24:36 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = "https://doctor-appointment-booking-backend-3ekf.onrender.com/"
+  const backendUrl = "https://doctor-appointment-booking-backend-3ekf.onrender.com"
 
   const [dToken, setDToken] = useState(localStorage.getItem("dToken") || "");
 
