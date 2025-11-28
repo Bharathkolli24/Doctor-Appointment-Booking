63ddc20cf9ef0a9a384b33ad835af12747b591c2
VIJAYA NAGA BHARATH KOLLI <bharathkolli24@gmail.com>
2025-11-28 12:26:09 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = '₹';
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://doctor-appointment-booking-backend-g9i2.onrender.com";
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(() => {
